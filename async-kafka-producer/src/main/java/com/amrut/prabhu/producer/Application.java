e2b5b8a0668e3fec1628090d3edd6cc06094f5f6
amrutprabhu42 <amrutprabhu42@gmail.com>
2021-06-11 13:46:09 +0200

    renamed start application classes

--- async-kafka-producer/src/main/java/com/amrut/prabhu/producer/Application.java
+++ async-kafka-producer/src/main/java/com/amrut/prabhu/producer/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProducerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
